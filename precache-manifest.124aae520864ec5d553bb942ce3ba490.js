6bb8524c62bfb3aa4245dc16c6799be59e07ee49
emir derouiche <memir0@hotmail.no>
2021-08-29 17:56:21 +0200

    Updates

---
+++ precache-manifest.124aae520864ec5d553bb942ce3ba490.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af9153a2273dea5813eeb620128308a",
+    "url": "/hjemmeside-v2/index.html"
+  },
+  {
+    "revision": "57b67847b5c872cae6fd",
+    "url": "/hjemmeside-v2/static/css/main.e7fed615.chunk.css"
+  },
+  {
+    "revision": "5f6d16e0b9c67d86ae4e",
+    "url": "/hjemmeside-v2/static/js/2.6f317db9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hjemmeside-v2/static/js/2.6f317db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b67847b5c872cae6fd",
+    "url": "/hjemmeside-v2/static/js/main.28775b1c.chunk.js"
+  },
+  {
+    "revision": "cf5ab42e7685198b7490",
+    "url": "/hjemmeside-v2/static/js/runtime-main.6c763d75.js"
+  },
+  {
+    "revision": "ef99e74fc7bf42d60ee0e1fa89ab6dcd",
+    "url": "/hjemmeside-v2/static/media/avatar.ef99e74f.png"
+  },
+  {
+    "revision": "8f42e018a9340b6e4e20161b06525308",
+    "url": "/hjemmeside-v2/static/media/cpp.8f42e018.png"
+  },
+  {
+    "revision": "f30ad7777b9268eebe501f028a83d84a",
+    "url": "/hjemmeside-v2/static/media/docker.f30ad777.png"
+  },
+  {
+    "revision": "297a556714d0fa002a3cd39eee629374",
+    "url": "/hjemmeside-v2/static/media/flask.297a5567.png"
+  },
+  {
+    "revision": "a7f48025c2fde4f2dc3cbee5f2a44ddf",
+    "url": "/hjemmeside-v2/static/media/graphql.a7f48025.png"
+  },
+  {
+    "revision": "711f654d966e29da576399cc8d873a9e",
+    "url": "/hjemmeside-v2/static/media/java.711f654d.png"
+  },
+  {
+    "revision": "d8ecb137a23039ded4413665af8e6b9d",
+    "url": "/hjemmeside-v2/static/media/landscape.d8ecb137.jpg"
+  },
+  {
+    "revision": "b7a14e27016e3787bc8a19efb4e82558",
+    "url": "/hjemmeside-v2/static/media/mysql.b7a14e27.png"
+  },
+  {
+    "revision": "1b29a538acb3efa07b2289d932b2daa9",
+    "url": "/hjemmeside-v2/static/media/php.1b29a538.png"
+  },
+  {
+    "revision": "50156c71fb2cf47d5cfe134231975922",
+    "url": "/hjemmeside-v2/static/media/postgresql.50156c71.png"
+  },
+  {
+    "revision": "76fc9aa252b70c510358bf5b9b4c1abc",
+    "url": "/hjemmeside-v2/static/media/python.76fc9aa2.png"
+  },
+  {
+    "revision": "f006806312dbf953a01945bc57c25740",
+    "url": "/hjemmeside-v2/static/media/pytorch_logo.f0068063.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/hjemmeside-v2/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "38a7ae8b00ac59b41ea9aff391bf9dd5",
+    "url": "/hjemmeside-v2/static/media/react_native.38a7ae8b.png"
+  },
+  {
+    "revision": "2a6fe6f63ace655f53e8893ab216cde1",
+    "url": "/hjemmeside-v2/static/media/sql.2a6fe6f6.png"
+  },
+  {
+    "revision": "8656e839fa33def4d00520fc84194a98",
+    "url": "/hjemmeside-v2/static/media/typescript.8656e839.png"
+  },
+  {
+    "revision": "bfaf5403b696f30136e4f4949a19e4e1",
+    "url": "/hjemmeside-v2/static/media/wip.bfaf5403.png"
+  }
+]);
